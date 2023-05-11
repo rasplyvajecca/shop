2f1f2b904b42f18da49009836f66de893387ee41
rasplyvajecca <rasplyvajeccaa@gmail.com>
2023-05-11 19:26:12 +0300

    add

--- table/models.py
+++ table/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Shop(models.Model):
     title = models.CharField('Название', max_length=50)
-    about = models.TextField('Описание', blank=True)
-    price = models.DecimalField('Цена', max_digits=6, decimal_places=2)
+    about = models.TextField('Описание', max_length=50)
+    price = models.DecimalField('Цена', max_digits=6, decimal_places=0)
     img = models.ImageField('Фото', upload_to='main/static/main/img/', blank=True)
 
     class Meta:
--- table/views.py
+++ table/views.py
@@ -4,6 +4,6 @@ from .models import Shop
 
 def table_home(request):
     table = Shop.objects.order_by('price')
-    return render(request, 'table/table_home.html', {'table': table})
+    return render(request, 'table/shop.html', {'table': table})
 
 
