3405476e11420a927eacda16d4b298cb1c7c9b14
rasplyvajecca <rasplyvajeccaa@gmail.com>
2023-05-14 01:51:18 +0300

    add

---
+++ users/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import auth
+from .forms import UserLoginForm, UserRegistrationForm
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return redirect('/')
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
